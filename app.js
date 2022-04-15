b0a0869689ebf1e7342e201bb436eaca7e417630
ijayesh20 <76622083+ijayesh20@users.noreply.github.com>
2022-04-15 17:26:17 +0530

    testing and error handling
    error handling done with testing

--- app.js
+++ app.js
@@ -7,11 +7,15 @@ var serverUrl = "https://api.funtranslations.com/translate/morse.json";
 function constructUrl(text) {
     return serverUrl + "?text=" + text;
 }
+function errorHandler(error) {
+    alert("Error occured! Try after some time.");
+}
 function inputClickHandler() {
     var inputText = textInput.value;
     fetch(constructUrl(inputText))
     .then(response => response.json())
     .then(json => divOutput.innerHTML = json.contents.translated)
+    .catch(errorHandler)
 }
 
 btnTranslate.addEventListener("click",inputClickHandler)
